4fe2e5939a9c6233974f8cd1900108debc650b94
Васильев Александр <wyndace.av@ya.ru>
2022-12-29 08:25:05 +0500

    Добавлена модель User

--- backend/utils/models.py
+++ backend/utils/models.py
@@ -4,3 +4,14 @@ from ..common.settings import *
 
 engine = create_engine(POSTGRESQL_SERVER)
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    first_name = Column(VARCHAR(255), nullable=True)
+    second_name = Column(VARCHAR(255), nullable=True)
+    phone = Column(DECIMAL(30), nullable=False)
+    username = Column(VARCHAR(255), nullable=True)
+    is_author = Column(Boolean, default=False, nullable=False)
