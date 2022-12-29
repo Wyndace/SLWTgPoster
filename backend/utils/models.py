cc44bbdbb5a8302ab2e61103dcdac35003bd7ccd
Васильев Александр <wyndace.av@ya.ru>
2022-12-29 08:39:40 +0500

    Добавлена модель Post

--- backend/utils/models.py
+++ backend/utils/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import DECIMAL, Boolean, create_engine, Column, Integer, VARCHAR, Text, ForeignKey
+from sqlalchemy import DECIMAL, TIMESTAMP, Boolean, create_engine, Column, Integer, VARCHAR, Text, ForeignKey
 from ..common.settings import *
 
 engine = create_engine(POSTGRESQL_SERVER)
@@ -22,3 +22,16 @@ class UsersNotify(Base):
 
     id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'),
                 nullable=False, index=True)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    text = Column(Text, nullable=False)
+    public_time = Column(TIMESTAMP, nullable=True)
+    is_repost = Column(Boolean, nullable=False, default=False)
+    repost_text = Column(Text, nullable=True)
+    repost_author = Column(VARCHAR(255), nullable=True)
+    repost_url = Column(VARCHAR(255), nullable=True)
